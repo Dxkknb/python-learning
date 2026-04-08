168a5b82e1568d42d0764adcf092077c6522c32d
Dxkknb <konanknbernard@gmail.com>
2026-04-08 23:04:43 +0000

    Work with variables: Make some calculus

---
+++ 01-foundations/01-variables/variables.py
@@ -0,0 +1,22 @@
+"""
+Nous allons apprendre à utiliser les variables.
+Connaître leur utilité et aussi les différents types de variables
+"""
+
+# Définition de variables
+
+age = 29
+name = "Konan Koffi N'guessan Bernard"
+job = 'Cil Engineer / Surveyor'
+
+print(f"Je suis {name} et j'ai {age} ans.\nEnfin je travaille en tant qu'un {job}.")
+
+# Calcul du périmètre et de l'ai d'un cercle
+
+roof = 3.5
+PI = 3.1415
+
+perimeter = 2 * PI * roof
+circle_area = PI * roof * roof
+
+print(f"Un cercle de rayon {roof} cm a pour:\n\tPérimètre: {perimeter}cm\n\tAire: {circle_area}cm2")
