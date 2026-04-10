a0dc5370d5be341461dc77658fb9d06a7d28def7
Dxkknb <konanknbernard@gmail.com>
2026-04-10 21:47:41 +0000

    Create a string with single and double quotation marks

---
+++ 01-foundations/02-datatypes/strings.py
@@ -0,0 +1,9 @@
+"""
+Strings in python are surrounded by either single quotation marks, or double quotation marks.
+"""
+
+name = "Konan Koffi"
+animal = 'Dog'
+
+print("Name:", name)
+print("Animal:", animal)
