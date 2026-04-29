0747ba4937755fab6fe32f3e049eb05f0995e0bc
Dxkknb <konanknbernard@gmail.com>
2026-04-29 08:48:51 +0000

    Exercises using conditions part 2

--- 01-foundations/03-conditions/exercises.py
+++ 01-foundations/03-conditions/exercises.py
@@ -85,3 +85,78 @@ elif operation == "/":
 
 if result:
     print(f"{num_1} {operation} {num_2} = {result}")
+
+# Exercise 6: Advanced Discount System
+import datetime
+
+purchase_amount = float(input("Provide the purchase amount: "))
+customer_type = input("Precise the customer type (regular, member, VIP): ").strip()
+
+today = datetime.datetime.today().weekday()
+
+if customer_type == "member":
+    if purchase_amount >= 100_000:
+        purchase_amount -= purchase_amount * 0.15
+    elif purchase_amount >= 50_000:
+        purchase_amount -= purchase_amount * 0.1
+elif customer_type == "VIP":
+    if purchase_amount >= 100_000:
+        purchase_amount -= purchase_amount * 0.2
+    elif purchase_amount >= 50_000:
+        purchase_amount -= purchase_amount * 0.3
+
+if today >= 5:
+    purchase_amount -= purchase_amount * 0.05
+
+print("------------------------- PURCHASE INVOICE ------------------")
+print(f"purchase amount: ${purchase_amount}")
+print(f"Customer type: {customer_type}")
+
+# Exercise 7: Tax Calculation System
+
+salary = float(input("Provide the salary: "))
+married = input("Are you married ? (Yes/No) ")
+number_of_children = int(input("How many children do you have? "))
+tax = 0
+
+if 100_001 <= salary <= 300_000:
+    tax = 0.1
+elif 300_001 <= salary <= 700_000:
+    tax = 0.2
+elif salary  >= 700_001:
+    tax = 0.3
+
+if married.lower() == "yes" and tax:
+    tax -= 0.05
+
+if number_of_children >= 3 and tax:
+    tax -= 0.1
+
+remained_salary = salary - tax * salary
+
+print(f"Remained salary: ${remained_salary}")
+
+# Exercise 8: Login System with Attempts
+correct_credentials = {
+    "username": "Alice",
+    "password": "12345"
+}
+print("------------------- LOGIN SYSTEM -------------------")
+
+total_attempts = 0
+
+while total_attempts < 3:
+    username = input("Username: ")
+    password = input("Paswword: ")
+    
+    total_attempts += 1
+
+    if (username == correct_credentials['username'] and password == correct_credentials['password']):
+        print("Access granted")
+        break
+    else:
+        if total_attempts == 3:
+            print("Account locked")
+        else:
+            print("Username or password is wrong.")
+            print(f"{3 - total_attempts} attempts left")
