0cf5b8e0ea95752f58c6f6dfa5405725e8eae6bf
Dxkknb <konanknbernard@gmail.com>
2026-05-04 21:11:10 +0000

    Dictionaries: Access dictionary items

--- 01-foundations/06-datatstructures/03-dictionaries/dictionnaries.py
+++ 01-foundations/06-datatstructures/03-dictionaries/dictionnaries.py
@@ -12,3 +12,23 @@ person = {
 }
 
 print(person, type(person))
+
+# Access items -> Error is key not exists
+name = person["name"]
+age = person["age"]
+job = person["job"]
+salary = person["salary"]
+city = person["city"]
+
+print(f"""
+Name: {name}
+Age: {age} years old
+Job: {job}
+Salary: ${salary}
+""")
+
+name_2 = person.get("model")
+if name_2 is None:
+    print("No 'model' in person data")
+else:
+    print('New name:', name_2)
