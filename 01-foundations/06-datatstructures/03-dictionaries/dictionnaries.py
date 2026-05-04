2a9ee711e5c481f89ace9dcf97f054e4b332eba0
Dxkknb <konanknbernard@gmail.com>
2026-05-04 21:02:52 +0000

    Create a dictionnary

---
+++ 01-foundations/06-datatstructures/03-dictionaries/dictionnaries.py
@@ -0,0 +1,14 @@
+"""
+Python Dictionnaries
+"""
+
+# Create a dictionnary
+person = {
+    "name": "Alice",
+    "age": 25,
+    "job": "Engineer",
+    "salary": 3500.50,
+    "city": "Abidjan"
+}
+
+print(person, type(person))
