b5de5c1195da9dce17b8c3c60d35b9171bea66d7
Dxkknb <konanknbernard@gmail.com>
2026-05-07 16:56:11 +0000

    Use cases of lambdas

--- 02-intermediate/02-lambdas/lamdbas.py
+++ 02-intermediate/02-lambdas/lamdbas.py
@@ -30,3 +30,52 @@ def my_map(func: Callable[[float], float], iterable:List[float]):
 
 cubed = my_map(lambda x: x**3, nums)
 print("Cubed:", cubed)
+
+"""
+COMMON USE CASES
+"""
+
+# TODO: Sorting with key
+
+students : List[tuple[str, int]] = [
+    ('Alice', 85),
+    ('Bob', 70),
+    ('Charlie', 95),
+    ('Emilia', 65),
+]
+
+copy_stds = students[:]
+copy_stds.sort(key=lambda student: student[1])
+print("Sorted students:", copy_stds)
+
+# TODO: Using with map()
+
+numbers = [1, 2, 3, 4, 5]
+squares = list(map(lambda x: x ** 2, numbers))
+print("Squared numbers:", squares)
+
+# TODO: Using with filter()
+
+numbers = [x * 2 for x in range(20)]
+five_multiples = list(filter(lambda x: x % 5 == 0, numbers))
+print("Five multiples:", five_multiples)
+
+# TODO: Using with reduce()
+from functools import reduce
+
+numbers = [1, 2, 3, 4]
+total = reduce(lambda a, b: a + b, numbers)
+print("total sum:", total)
+
+# TODO: Dictionary sorting
+
+products = {
+    "Phone": 800,
+    "Laptop": 1500,
+    "Tablet": 600
+}
+sorted_products = sorted(
+    products.items(),
+    key=lambda item: item[1]
+)
+print(sorted_products)
