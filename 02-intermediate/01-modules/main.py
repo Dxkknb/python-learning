f4bb547e61e4817a29dec41a8c9683e5208bbe3a
Dxkknb <konanknbernard@gmail.com>
2026-05-07 15:38:58 +0000

    More about python modules

--- 02-intermediate/01-modules/main.py
+++ 02-intermediate/01-modules/main.py
@@ -1,4 +1,5 @@
 import fibo
+from utils import get_name, greeting as greet
 
 def main() -> None:
     # Using functions in a module
@@ -6,6 +7,14 @@ def main() -> None:
 
     result = fibo.fib2(100)
     print(result)
+
+    # More on Modules
+    firstname = input("Your firstname: ")
+    lastname = input("Your lastname: ")
+
+    if firstname or lastname:
+        fullname = get_name(firstname, lastname)
+        print(greet(fullname))
 
 if __name__ == "__main__":
     main()
---
+++ 02-intermediate/01-modules/utils.py
@@ -0,0 +1,5 @@
+def greeting(name: str) -> str:
+    return f"Hello {name}"
+
+def get_name(firstname: str, lastname: str) -> str:
+    return f"{lastname.capitalize()} {firstname.capitalize()}"
