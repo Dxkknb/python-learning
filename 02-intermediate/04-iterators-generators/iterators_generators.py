4f637696d9d531cc06fe2213a66fba5065fcf727
Dxkknb <konanknbernard@gmail.com>
2026-05-08 16:54:15 +0000

    Python Iterator: Tuple Iterator

--- 02-intermediate/04-iterators-generators/iterators_generators.py
+++ 02-intermediate/04-iterators-generators/iterators_generators.py
@@ -78,3 +78,27 @@ iterator = ListIterator(numbers)
 
 for num in iterator:
     print(num)
+
+# Tuple Iterator
+
+class TupleIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+
+        value = self.data[self.index]
+        self.index += 1
+
+        return value
+
+names = ('Annie', 'Julie', 'Jack', 'John')
+
+for name in TupleIterator(names):
+    print(name)
